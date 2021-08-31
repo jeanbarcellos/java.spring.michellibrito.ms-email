c2b93c5cbbba271dbef4ddfe533bc93a3323de5a
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-30 22:53:40 -0300

    Primeiro controller

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping(value = "/")
+    public String index() {
+        return "Olá mundo VsCode!";
+    }
+}
