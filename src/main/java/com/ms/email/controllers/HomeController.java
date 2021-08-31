c834f7b7607fb3cadd1935aac17c0c57cfba079b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-30 23:01:04 -0300

    Reorganização dos pacotes

--- src/main/java/com/ms/email/controllers/HomeController.java
+++ src/main/java/com/ms/email/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controllers;
+package com.ms.email.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
