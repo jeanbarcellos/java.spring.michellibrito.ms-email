cb720c8951764a6dac8e5e00fb7bdd2629cd5aaa
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-09-09 00:40:35 -0300

    Update ID do model

--- src/main/java/com/ms/email/repositories/EmailRepository.java
+++ src/main/java/com/ms/email/repositories/EmailRepository.java
@@ -1,7 +1,9 @@
 package com.ms.email.repositories;
 
+import java.util.UUID;
+
 import com.ms.email.models.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 }
